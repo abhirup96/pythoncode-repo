439a606ef18c73d63fe8891274161d126128f1f7
abhirup96 <purkayastha.abhirup@gmail.com>
2022-05-19 13:05:19 +0000

    Add files via upload

---
+++ ap.py
@@ -0,0 +1,10 @@
+#Write a program to find largest of two numbers.
+
+a=input("Enter a number :")
+b=input("Enter b number :")
+if (a>b):
+	print("a is greater than b")
+elif (a==b):
+	print("a is equal to b")
+else:
+	print("b is greater than a")
